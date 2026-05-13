ea2f81d531f584a7b06d4077a44d0d47a1940228
joao <jovmamikl@gmail.com>
2026-05-13 09:44:06 -0300

    feat: update ChatController to handle SQL generation and response structure

--- src/main/java/com/AgentAIEstoque/application/controller/ChatController.java
+++ src/main/java/com/AgentAIEstoque/application/controller/ChatController.java
@@ -1,12 +1,13 @@
 package com.AgentAIEstoque.application.controller;
 
-import java.util.Map;
+import java.util.*;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import com.AgentAIEstoque.application.service.AgenteTextToSqlService;
+import com.AgentAIEstoque.application.service.DatabaseExecutionService;
 
 import lombok.AllArgsConstructor;
 
@@ -20,19 +21,35 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class ChatController {
 
     private final AgenteTextToSqlService agenteService;
+    private final DatabaseExecutionService databaseService;
 
     
-    @PostMapping("/gerar-sql")
-    public ResponseEntity<Map<String, String>> gerarSql(@RequestBody Map<String, String> request) {
+    @PostMapping("/perguntar")
+    public ResponseEntity<Map<String, Object>> perguntarAoAgente(@RequestBody Map<String, String> request) {
        
         String pergunta = request.get("pergunta");
         if(pergunta == null || pergunta.isBlank()){
             return ResponseEntity.badRequest().body(Map.of("erro", "A pergunta não pode estar vazia."));
         }
-        
-        String sql = agenteService.traduzirPerguntaParaSql(pergunta);
 
-        return ResponseEntity.ok(Map.of("sql_gerado", sql));
+        Map<String, Object> respostaFinal = new HashMap<>();
+
+        try {
+            String sqlGerado = agenteService.traduzirPerguntaParaSql(pergunta);
+            respostaFinal.put("sql_utilizado", sqlGerado);
+
+            List<Map<String, Object>> dados = databaseService.executarSqlDinamico(sqlGerado);
+            respostaFinal.put("dados", dados);
+
+            return ResponseEntity.ok(respostaFinal);
+        } catch (SecurityException se) {
+
+            return ResponseEntity.status(403).body(Map.of("erro_seguranca", se.getMessage()));
+        } catch (Exception e) {
+            
+            return ResponseEntity.internalServerError().body(Map.of("erro_processamento", e.getMessage()));
+        }
+        
     }
     
 }
