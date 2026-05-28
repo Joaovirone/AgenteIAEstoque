1dac9841d10c8e6aae73b4b69f6c9bc497974823
joao <jovmamikl@gmail.com>
2026-05-28 11:27:12 -0300

    feat: adicionado mais endopoints para buscar, atualizar e deletar os produtos

--- src/main/java/com/AgentAIEstoque/application/controller/ProdutoController.java
+++ src/main/java/com/AgentAIEstoque/application/controller/ProdutoController.java
@@ -1,18 +1,21 @@
 package com.AgentAIEstoque.application.controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.AgentAIEstoque.application.dto.ProdutoRequestDTO;
 import com.AgentAIEstoque.application.dto.ProdutoResponseDTO;
-import com.AgentAIEstoque.application.entity.Produto;
 import com.AgentAIEstoque.application.service.ProdutoService;
 
 import lombok.AllArgsConstructor;
@@ -26,15 +29,30 @@ public class ProdutoController {
     
     @PostMapping
     public ResponseEntity<ProdutoResponseDTO> cadastrarProduto(@RequestBody ProdutoRequestDTO request) {
-      
         ProdutoResponseDTO response = produtoService.salvarProduto(request);
-
-            return ResponseEntity.status(HttpStatus.CREATED).body(response);
-        
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping
     public ResponseEntity<List<ProdutoResponseDTO>> listarProdutos() {
         return ResponseEntity.ok(produtoService.listarTodos());
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoResponseDTO> buscarPorId(@PathVariable UUID id) {
+        return ResponseEntity.ok(produtoService.buscarPorId(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProdutoResponseDTO> atualizarProduto(@PathVariable UUID id, @RequestBody ProdutoRequestDTO request) {
+        ProdutoResponseDTO response = produtoService.atualizarProduto(id, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluirProduto(@PathVariable UUID id) {
+        produtoService.excluirProduto(id);
+        return ResponseEntity.noContent().build();
     }
 }
